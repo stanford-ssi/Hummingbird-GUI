781e486f7859e978ce6954b6292f27c6eded2bcd
orangejuice1513 <juliaj94@stanford.edu>
2025-05-19 20:46:44 -0700

    measurements, left panel

--- GUI/left_panel.py
+++ GUI/left_panel.py
@@ -1,5 +1,6 @@
 """ Stage + experiment + quick file control """
 import tkinter as tk
+from measure_widget import MeasureWidget
 
 class LeftPanel:
     frame: tk.Frame
@@ -8,8 +9,16 @@ class LeftPanel:
         # Create frame
         self.frame = tk.Label(parent, background='blue')
 
-        # PT, TT, and LC measurements
+        # top frame
+        self.topframe = tk.Frame(self.frame)
+        self.title = tk.Label(self.topframe, text = 'Measurements', font=('Comic Sans MS', 26))
 
+        # PT, TT, and LC measurements
+        self.pt1 = MeasureWidget(self.frame, 'PT1', '[Pa]')
 
     def render(self):
+        self.title.pack(fill = 'x')
+        self.topframe.pack(fill = 'x')
+        self.pt1.render()
         self.frame.pack(side='left', fill='both', expand=True)
+
---
+++ GUI/measure_widget.py
@@ -0,0 +1,27 @@
+import tkinter as tk 
+
+class MeasureWidget:
+    frame: tk.Frame
+    partname: str
+    units: str
+
+    def __init__(self, parent, partname, units, default_entry = 0.0):
+        self.frame = tk.Label(parent) #makes frame
+        self.partname = partname
+        self.units = units
+
+        self.partname_box = tk.Label(self.frame, text = self.partname, font = ("Comic Sans MS", 20))
+        self.measurement_box = tk.Label(self.frame, text = str(default_entry), font = ("Comic Sans MS", 20))
+        self.units_box = tk.Label(self.frame, text = self.units, font = ("Comic Sans MS", 20))
+
+    def render(self):
+        # locations of the part name, measurements, and units 
+        self.partname_box.pack(side = 'left', padx = 30)
+        self.measurement_box.pack(side = 'left', padx = 30)
+        self.units_box.pack(side = 'left', padx = 30)
+
+        self.frame.pack(fill = 'x', side = 'top')
+
+
+
+
